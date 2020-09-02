f8e75dbaeb1d30dd11140066098ca062c646ac0c
przemek <przemek.gonda@gmail.com>
2020-09-03 00:05:42 +0200

    app component

---
+++ src/app/app.module.ts
@@ -0,0 +1,65 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatTableModule} from '@angular/material/table';
+import { TodolistComponent } from './views/todolist/todolist.component';
+import { MatSortModule } from '@angular/material/sort';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NewTaskDialogComponent } from './dialogs/new-task-dialog/new-task-dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { EditTaskDialogComponent } from './dialogs/edit-task-dialog/edit-task-dialog.component';
+import {StorageServiceModule} from 'ngx-webstorage-service';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodolistComponent,
+    NewTaskDialogComponent,
+    EditTaskDialogComponent,
+  ],
+  entryComponents: [
+    NewTaskDialogComponent,
+    EditTaskDialogComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatSortModule,
+    FormsModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    StorageServiceModule,
+    MatTooltipModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
